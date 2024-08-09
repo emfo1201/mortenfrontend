902624a2785c0e8679678033d06621f5505a18e7
Emelie Fogelström <fogelstrme@gmail.com>
2024-08-09 14:14:25 +0200

    fixing S3 bucket for update player

--- src/components/Players/Player/AddNewPlayer.js
+++ src/components/Players/Player/AddNewPlayer.js
@@ -45,7 +45,7 @@ const AddNewPlayer = ({ handleOpenDialog, handleCloseDialog, openDialog }) => {
     
     console.log("selectedCategoriesToSend:", selectedCategoriesToSend);
     
-    data.append('categories', JSON.stringify(selectedCategoriesToSend));
+    data.append('categories', categoriesToSend);
     
 
     // Add images
